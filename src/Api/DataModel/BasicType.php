4f89b91a4d6e426643aa37d01e2c26341d78050b
Marlon Saglia <marlon@ramsalt.com>
2020-06-18 14:40:27 +0200

    Add week and day period types

--- src/Api/DataModel/BasicType.php
+++ src/Api/DataModel/BasicType.php
@@ -5,6 +5,10 @@ namespace ConnectID\Api\DataModel;
 
 abstract class BasicType extends BasicData implements BasicTypeInterface {
 
+  public const PERIOD_DAY = 'days';
+
+  public const PERIOD_WEEK = 'weeks';
+
   public const PERIOD_MONTH = 'months';
 
   public const PERIOD_YEAR = 'years';
